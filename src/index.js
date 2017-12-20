81b07bdb7c05bdeec7629d74d7b745b5f2e2d891
wuding <wuding129@163.com>
2017-12-21 06:53:26 +0800

    redux devtools

--- src/index.js
+++ src/index.js
@@ -12,14 +12,19 @@ registerServiceWorker();
 
 import React from 'react'
 import ReactDOM from 'react-dom'
-import {createStore, applyMiddleware} from 'redux'
+import {createStore, applyMiddleware, compose} from 'redux'
 import thunk from 'redux-thunk'
 
 import './index.css'
 import App from './App'
 import {addGun, counter, removeGun, addGunAsync} from "./store";
 
-const store = createStore(counter, applyMiddleware(thunk))
+
+const reduxDevtools = window.devToolsExtension?window.devToolsExtension(): f=>f
+const store = createStore(counter, compose(
+  applyMiddleware(thunk),
+  reduxDevtools)
+)
 
 function render(){
   ReactDOM.render(<App store={store} addGun={addGun} removeGun={removeGun} addGunAsync={addGunAsync}/>, document.getElementById('root'))
